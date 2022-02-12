37e98d3c80162d7be2917ec2309e73a5ddc6dae4
Enchan <enchant0130@gmail.com>
2022-02-12 11:29:39 +0900

    [Update #16] main.py (basically learning process)

--- main.py
+++ main.py
@@ -3,7 +3,13 @@
 #
 import sys
 from typing import List
+
+import numpy as np
+from numpy import ndarray
+from src.activator import Relu
 from src.mnist_loader import get_datamodel
+from src.network import NeuralNetwork
+from src.optimizer import SGD
 
 
 def main(args: List[str]) -> int:
@@ -13,6 +19,32 @@ def main(args: List[str]) -> int:
     print(f"\t画像:{x_train.shape}\n\t教師:{t_train.shape}")
     print("テストデータ:")
     print(f"\t画像:{x_test.shape}\n\t教師:{t_test.shape}")
+
+    # ネットワーク生成
+    network = NeuralNetwork(784, 10)
+    network.add_layer(100, Relu())
+    print(network)
+
+    # オプティマイザ生成
+    lr = 0.1
+    optimizer = SGD(lr)
+
+    # 学習開始!
+    batch_size = 100
+    train_size = x_train.shape[0]
+    step_count = 10000
+    for step in range(step_count):
+        print(f"Step {step}:")
+
+        # ミニバッチの生成
+        batch_indices = np.random.choice(train_size, batch_size)
+        x_batch: ndarray = x_train[batch_indices]
+        t_batch: ndarray = t_train[batch_indices]
+
+        gradient = network.gradient(x_batch, t_batch)
+        optimizer.update(gradient)
+
+    print(network)
 
     return 0
 
